d95841356eed1e55769b968fc8d2ddc61cb26e4b
cookie <cookie@twosun.com>
2021-11-22 19:12:25 +0900

    Upt. 못찾으면 1초 기다렸다가 다시 찾기

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 var HID = require('node-hid');
 var devices = HID.devices();
-const barcodeDevice = devices.find(item => item.vendorId == '1204' && item.productId == '256')
-var device = new HID.HID(barcodeDevice.vendorId, barcodeDevice.productId);
-device.on("data", function(data) {
-  console.log(data)
-});
+
+function findDeviceLoop() {
+  const barcodeDevice = devices.find(item => item.vendorId == '1204' && item.productId == '256')
+  if (!barcodeDevice) {
+    console.log("Not found barcode device")
+    setTimeout(function() {
+      findDeviceLoop()
+    }, 1000)
+    return false;
+  }
+
+  var device = new HID.HID(barcodeDevice.vendorId, barcodeDevice.productId);
+  device.on("data", function(data) {
+    console.log(data)
+  });
+
+  return true
+}
+
+setTimeout(function() {
+  findDeviceLoop()
+})
